092482490cfc98b1478420e0ae8837f46c49a25e
LAPTOP-9DCKQ4UQ\Utilizador <1170425@isep.ipp.pt>
2017-11-21 12:28:58 +0000

    commit certo

---
+++ Grande_premio.java
@@ -0,0 +1,16 @@
+
+package grande_premio;
+
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Grande_premio {
+   static Scanner in = new Scanner(System.in);
+   
+    public static void main(String[] args) {
+    String nome_ficheiro;    
+    nome_ficheiro=JOptionPane.showInputDialog("introduza o nome do ficheiro");
+    Scanner in=new Scanner(new File(“nome_ficheiro”));   
+    }
+    
+}
